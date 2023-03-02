4c076e082f188f4b9af29252ab34adeec5d2f216
mbstevenson96 <mbstevenson96@gmail.com>
2023-03-02 15:28:32 -0500

    comnplete exercise for basic operators

--- 01-Fundamentals-Part-1/starter/js/app.js
+++ 01-Fundamentals-Part-1/starter/js/app.js
@@ -6,4 +6,26 @@
 // console.log(true);
 
 let javascriptIsFun = true
-console.log(javascriptIsFun);  
+// console.log(javascriptIsFun);  
+
+
+const now = 2037
+const ageJonas = now - 1991
+const ageSarah = now - 2018
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2** 3);
+
+const firstName = 'Jonas'
+const lastName = 'Schmedtmann'
+console.log(firstName + ' ' + lastName);
+
+
+// Assignment operators
+let x = 10 + 5
+x += 10 // x = x + 10 = 25
+x *= 4 // x = x * 4 = 100
+console.log(x);
+
+// Comnparison operators
+console.log(ageJonas > ageSarah); // >, <, >=, <=
