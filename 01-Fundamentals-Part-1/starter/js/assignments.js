ba3964d7d893f466c1929bec91f8b9c7305d5aea
mbstevenson96 <mbstevenson96@gmail.com>
2023-03-02 14:51:42 -0500

    complete exercise for let and const

--- 01-Fundamentals-Part-1/starter/js/assignments.js
+++ 01-Fundamentals-Part-1/starter/js/assignments.js
@@ -17,9 +17,17 @@ let population = 336
 // 2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console
 
 let isIsland = false
-let language
+// let language
 
-console.log(typeof isIsland);
-console.log(typeof country);
-console.log(typeof population);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof country);
+// console.log(typeof population);
+// console.log(typeof language);
+
+
+// LET AND CONST
+// 1. Set the value of 'language' to the language spoken where you live (some countries have multiple languages, but just choose one)
+//   2. Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
+//   3. Try to change one of the changed variables now, and observe what happens
+
+const language = 'English'
